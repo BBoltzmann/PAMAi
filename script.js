e430a7ad0f6dc1b7945bbeb1511adb910b162f25
BBoltzmann <woleoshoifeoluwa@gmail.com>
2023-11-13 09:21:03 +0100

    update

--- script.js
+++ script.js
@@ -1,57 +1,46 @@
-document.getElementById('questionnaireForm').addEventListener('submit', function (e) {
+document.getElementById('questionnaireForm').addEventListener('submit', function(e) {
     e.preventDefault();
-
     const formData = new FormData(this);
     const selectedProfile = determineProfile(formData);
     displayProfileChart(selectedProfile);
 });
 
 function determineProfile(formData) {
-    const peakUsage = formData.get('peakUsage');
-    const energyGeneration = parseFloat(formData.get('energyGeneration')) || 0;
-    const conservation = formData.get('energyConservation');
-    const pricingReaction = formData.get('pricingReaction');
-
-    if (peakUsage === 'morning' && energyGeneration > 5) {
-        return 'Sunrise Savers';
-    } else if (peakUsage === 'night' && pricingReaction === 'notAtAll') {
-        return 'Night Owls';
-    } else if (conservation && energyGeneration > 10) {
+    const energyGeneration = parseFloat(formData.get('energyGeneration'));
+    if (energyGeneration > 5) {
         return 'Eco Warriors';
-    } else if (peakUsage === 'daylight' && pricingReaction === 'veryLikely') {
-        return 'Daylight Dynamos';
-    } else if (peakUsage === 'evening' && pricingReaction === 'somewhatLikely') {
-        return 'Twilight Guardians';
+    } else if (formData.get('peakUsage') === 'night') {
+        return 'Night Owls';
     } else {
-        return 'Morning Gliders';
+        return 'Sunrise Savers'; // Default profile
     }
 }
 
 function displayProfileChart(profileName) {
     const chartContainer = document.getElementById('profileChart');
     chartContainer.innerHTML = generateChartHtml(profileName);
-    addHoverEffects();
 }
 
 function generateChartHtml(profileName) {
-    let chartHtml = `<div class="chart ${profileName}">`;
-    let totalGeneration = 0;
+    let chartHtml = `<div class="chart-title">${profileName}</div>`;
+    chartHtml += `<div class="chart ${profileName}">`;
 
+    let totalGeneration = 0;
     for (let hour = 0; hour < 24; hour++) {
         const loadHeight = Math.random() * 100;
         const generationHeight = Math.random() * 100;
         totalGeneration += generationHeight;
+
         chartHtml += `<div class="time-slot">`;
-        chartHtml += `<div class="load-bar" style="height: ${loadHeight}%;" data-value="${loadHeight}"></div>`;
-        chartHtml += `<div class="generation-bar" style="height: ${generationHeight}%;" data-value="${generationHeight}"></div>`;
+        chartHtml += `<div class="load-bar" style="height: ${loadHeight}%;"></div>`;
+        chartHtml += `<div class="generation-bar" style="height: ${generationHeight}%;"></div>`;
         chartHtml += `</div>`;
     }
 
     const averageGeneration = totalGeneration / 24;
     chartHtml += determineRates(averageGeneration);
     chartHtml += `</div>`;
-    chartHtml += `<div class="axis-label-y">Energy (kWh)</div>`;
-    chartHtml += `<div class="axis-label-x">Time (Hours)</div>`;
+    chartHtml += generateAxisLabels();
     return chartHtml;
 }
 
@@ -60,6 +49,7 @@ function determineRates(averageGeneration) {
     for (let hour = 0; hour < 24; hour++) {
         const generationHeight = Math.random() * 100;
         let rateClass = '';
+
         if (generationHeight < averageGeneration * 0.5) {
             rateClass = 'more-expensive-line';
         } else if (generationHeight > averageGeneration * 1.3) {
@@ -67,17 +57,25 @@ function determineRates(averageGeneration) {
         } else {
             rateClass = 'normal-rate-line';
         }
+
         ratesHtml += `<div class="rate-line ${rateClass}" style="left: ${hour * (100 / 24)}%;"></div>`;
     }
     return ratesHtml;
 }
 
-function addHoverEffects() {
-    const bars = document.querySelectorAll('.load-bar, .generation-bar');
-    bars.forEach(bar => {
-        bar.addEventListener('mouseenter', (event) => {
-            const value = event.target.dataset.value;
-            alert('Value: ' + value);
-        });
-    });
+function generateAxisLabels() {
+    let yAxisHtml = '<div class="y-axis-labels">';
+    for (let i = 0; i <= 100; i += 25) {
+        yAxisHtml += `<div class="y-axis-marker">${i}</div>`;
+    }
+    yAxisHtml += '</div>';
+
+    let xAxisHtml = '<div class="x-axis-labels">';
+    for (let hour = 0; hour < 24; hour++) {
+        let timeLabel = hour < 10 ? '0' + hour + ':00' : hour + ':00';
+        xAxisHtml += `<div class="x-axis-label">${timeLabel}</div>`;
+    }
+    xAxisHtml += '</div>';
+
+    return yAxisHtml + xAxisHtml;
 }
